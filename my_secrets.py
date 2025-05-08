9dc686744d9e0bfde61c63c502d7fa2f1ee606f4
Srimayi <6.srimayi12@gmail.com>
2025-05-08 23:59:55 +0530

    Removed my_secrets.py from repo and added to .gitignore

--- my_secrets.py
+++
@@ -1,8 +0,0 @@
-import secrets
-MYSQL_HOST = 'localhost'
-MYSQL_PORT = 3306
-MYSQL_USER = 'Srimayi'
-MYSQL_PASSWORD = 'rms@jk_0613#'
-MYSQL_DB = 'ecommerceinventory'
-secret_key = secrets.token_hex(16)
-SECRET_KEY = secret_key
